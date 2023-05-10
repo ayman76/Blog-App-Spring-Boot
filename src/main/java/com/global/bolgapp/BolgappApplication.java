efba5207ad7740521e4664e64c06a40aaed2eea6
Ayman <a.m_ayman9@outlook.com>
2023-05-10 14:23:07 +0200

    Creating the structure of the project

---
+++ src/main/java/com/global/bolgapp/BolgappApplication.java
@@ -0,0 +1,13 @@
+package com.global.bolgapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolgappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolgappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/global/bolgapp/BolgappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global.bolgapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolgappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
